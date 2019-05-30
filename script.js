bad35f3c1ec869bf64178d04e1488002c04e4c87
thalitaaureliano <thalitaaurel@gmail.com>
2019-05-29 22:11:10 -0300

    posting

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-const comidas = [
-  {
-    nome: "Batata frita",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/Batata-frita.jpg"
-  },
-  {
-    nome: "Macarronada",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/macarronada.jpg"
-  },
-  {
-    nome: "Falafel",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/falafel.jpg"
-  },
-  {
-    nome: "Creme de abóbora",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/creme-de-abobora.jpg"
-  },
-
-]
-
 const container = document.querySelector('#items-cardapio')
-// fetch(`localhost:3001`)
-//     .then((response) =>{
-//         return response.json();
-//     })
-//     .then((data) =>{
+ fetch(`http://localhost:4000/comidas`)
+     .then((response) =>{
+         return response.json();
+     })
+     .then((data) =>{
 
-        comidas.forEach(prato => {
+        data.pratosFavoritos.forEach(prato => {
             console.log(prato)
 
             const mediaItem = document.createElement('div');
@@ -45,8 +21,8 @@ const container = document.querySelector('#items-cardapio')
               container.appendChild(mediaItem);
         }
         )
-    // }
-    // )
-    // .catch((erro)=>{
-    //     console.log(erro)
-    // }
+     }
+     )
+     .catch((erro)=>{
+         console.log(erro)
+     })
